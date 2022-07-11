f6ad80a70730b8bf22e6db886c42e9e550eee6ec
Luiss Resources <luissresources@gmail.com>
2022-07-11 08:30:34 -0500

    Add component CardMovie and PageWrapper. Update App.js with clean code

--- src/components/CardMovie.js
+++ src/components/CardMovie.js
@@ -3,26 +3,22 @@ import React from 'react';
 function CardMovie(props) {
   return (
     <div class="movie-item-style-2">
-      <img src="images/uploads/mv1.jpg" alt="" />
+      <img src={props.image} alt="" />
       <div class="mv-item-infor">
         <h6>
-          <a href="moviesingle.html">oblivion <span>(2012)</span></a>
+          <a href="moviesingle.html">{props.title} <span>({props.year})</span></a>
         </h6>
-        <p class="rate"><i class="ion-android-star"></i><span>8.1</span> /10</p>
+        <p class="rate"><i class="ion-android-star"></i><span>{props.rate}</span> /10</p>
         <p class="describe">
-          Earth's mightiest heroes must come together and learn to fight as a team
-          if they are to stop the mischievous Loki and his alien army from enslaving
-          humanity...
+          {props.children}
         </p>
         <p class="run-time">
-          Run Time: 2h21’ . <span>MMPA: PG-13 </span> .
-          <span>Release: 1 May 2015</span>
+          Run Time: {props.duration} . <span>MMPA: PG-13 </span> .
+          <span>Release: {props.date}</span>
         </p>
-        <p>Director: <a href="localhost">Joss Whedon</a></p>
+        <p>Director: <a href="localhost">{props.director}</a></p>
         <p>
-          Stars: <a href="localhost">Robert Downey Jr.,</a>
-          <a href="localhost">Chris Evans,</a>
-          <a href="localhost"> Chris Hemsworth</a>
+          Stars: <a href="localhost">{props.starts},</a>
         </p>
       </div>
     </div>
